11a2317118d403207fa9ea3c5771263a41e868c7
ADoff <161216087+AdequacyOFF@users.noreply.github.com>
2025-03-29 17:13:41 +0300

    add tables reservations and places

---
+++ migrations/sql/20250329_0005_add_parcking_places_table_c1aaf4.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS parcking.places (
+    id SERIAL PRIMARY KEY,
+    car_number VARCHAR(20) NULL,
+    owner UUID NULL,
+    is_busy BOOLEAN NOT NULL,
+    CONSTRAINT fk_places_owner FOREIGN KEY (owner)
+    REFERENCES accounts.user(id)
+    ON DELETE SET NULL
+);
